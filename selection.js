18afcfdf071fc9bea0bf61a8b828401790eb638e
‘HunterWeis’ <hunter.weis@stonybrook.edu>
2015-11-08 03:11:16 -0500

    final V1

--- selection.js
+++ selection.js
@@ -1,8 +1,13 @@
 chrome.extension.onMessage.addListener(function(request,sender,sendResponse){
 	if (request.method && request.method == "getSelection")
 	{
+		var ourString = window.getSelection().toString();
+		var regexp = new RegExp('#([^\\s]*)','g');
+		var postText = ourString.replace(regexp, '');
+
+
 		console.log(window.getSelection().toString());
-		sendResponse({data:window.getSelection().toString()});
+		sendResponse({data:postText});
 	}
 	else{
 		sendResponse({data:"Nothing highlighted."});
