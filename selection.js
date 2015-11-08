b27807ab001115278c526e2a599899413c009b89
‘HunterWeis’ <hunter.weis@stonybrook.edu>
2015-11-07 20:14:33 -0500

    add bootstrap and make the shit look beautiful

--- selection.js
+++ selection.js
@@ -1,6 +1,8 @@
 chrome.extension.onMessage.addListener(function(request,sender,sendResponse){
 	if (request.method && request.method == "getSelection")
 	{
+
+		console.log(window.getSelection().toString());
 		sendResponse({data:window.getSelection().toString()});
 		//console.log({data:window.getSelection().toString()});
 		//sendResponse({data: "cocks"});
